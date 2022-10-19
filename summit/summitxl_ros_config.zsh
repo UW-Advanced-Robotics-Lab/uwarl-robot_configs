221ff688628e780a7eb422d023a34b6e2b2f1265
uwarl <UWARL MXE210 18 (Jack Xu)>
2022-10-19 16:06:58 -0400

    [Jack's autoboot scripts] + including readme instructions - separating wam and summit configs

---
+++ summit/summitxl_ros_config.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+### ROS RELATED ###
+## VAR ##
+export ROS_CATKIN_WS="$HOME/UWARL_catkin_ws"
+export UWARL_CONFIGS="$HOME/uwarl-robot_configs/summit"
+
+## ROS Env ##
+echo "[UWARL-ROS-Config] Sourcing ros ws @ $ROS_CATKIN_WS"
+source /opt/ros/melodic/setup.zsh
+source $ROS_CATKIN_WS/devel/setup.zsh
+source $UWARL_CONFIGS/summitxl_params.env
+
+## ROS Networks ##
+#export ROS_MASTER_URI=http://192.168.1.11:11311/
+#export ROS_IP=192.168.1.11
+
+echo "[UWARL-ROS-Config] ROS MASTER @ " $ROS_MASTER_URI
+echo "[UWARL-ROS-Config] ROS MASTER @ " $ROS_IP
+
+## Alias Commnads ##
+## kill processor:
+alias kill-ros="ps aux  | grep -e ros | awk '{print $2}' | xargs -i -exec kill -9 {}"
+alias source_ws="source $ROS_CATKIN_WS/devel/setup.zsh"
+
+
