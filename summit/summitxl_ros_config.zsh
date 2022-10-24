ee263ad64bff96b7c76d44e1a82f0a1d94860b88
uwarl <UWARL MXE210 18 (Jack Xu)>
2022-10-24 11:20:07 -0400

    + git batch shortcuts: fetch & pull

--- summit/summitxl_ros_config.zsh
+++ summit/summitxl_ros_config.zsh
@@ -21,6 +21,10 @@ echo "[UWARL-ROS-Config] ROS MASTER @ " $ROS_IP
 ## Alias Commnads ##
 ## kill processor:
 alias kill-ros="ps aux  | grep -e ros | awk '{print $2}' | xargs -i -exec kill -9 {}"
+## ROS src:
 alias source_ws="source $ROS_CATKIN_WS/devel/setup.zsh"
+## Git Batch Shortcuts:
 alias git-status-all="find . -maxdepth 1 -type d -execdir sh -c 'cd {}; echo \"[Git {}]\"; git status; git remote -v; echo \"----------\n\"' \;"
 alias git-push-all="find . -maxdepth 1 -type d -execdir sh -c 'cd {}; echo \"[Pushing {}]\"; git push; echo \"----------\n\"' \;"
+alias git-fetch-all="find . -maxdepth 1 -type d -execdir sh -c 'cd {}; echo \"[Fetching {}]\"; git fetch; echo \"----------\n\"' \;"
+alias git-pull-all="find . -maxdepth 1 -type d -execdir sh -c 'cd {}; echo \"[Fetching {}]\"; git pull; echo \"----------\n\"' \;"
