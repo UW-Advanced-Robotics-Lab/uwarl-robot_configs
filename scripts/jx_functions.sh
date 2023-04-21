4de775c037620a56076b9ede8294aabaf67ef55c
Jack (Desktop PC) <projectbyjx@gmail.com>
2023-04-20 22:35:40 -0400

    + function updates passing by name indirectly

--- scripts/jx_functions.sh
+++ scripts/jx_functions.sh
@@ -3,7 +3,7 @@ source "$HOME/uwarl-robot_configs/scripts/common.sh"
 
 function find_file_or_prompt_for_selections() {
     # find bag file:
-    selected_file=$1 # return placeholder
+    local placeholder_name=$1 # returns
     local search_directory=$2 #~/.ros/bag_replay_recorder_files
     local bag_file_pattern=$3 #"$file_prefix*.bag"
     
@@ -16,8 +16,8 @@ function find_file_or_prompt_for_selections() {
         ic " > Found: ${bag_file_names[@]}"
         selected_file=$bag_file_names
     elif [ $N -gt 1 ]; then
-        ic_wrn "> Multiple files found for pattern: $bag_file_pattern @ $search_directory"
-        ic_wrn "> Please select the listed index to play:"
+        ic_wrn " > Multiple files found for pattern: $bag_file_pattern @ $search_directory"
+        ic_wrn " > Please select the listed index to play:"
         select selected_file in "${bag_file_names[@]}"; do
             if [ $REPLY -gt $N ]; then
                 ic_err "Invalid option: ($REPLY)" && exit 1
@@ -30,5 +30,29 @@ function find_file_or_prompt_for_selections() {
         ic_err " x- No file found for pattern: $bag_file_pattern @ $search_directory"
         exit 1
     fi
+    # indirect:
+    set -A "$placeholder_name" "${selected_file[@]}"
 
+}
+
+function find_all_files() {
+    # find bag file:
+    local placeholder_name=$1
+    local search_directory=$2 #~/.ros/bag_replay_recorder_files
+    local bag_file_pattern=$3 #"$file_prefix*.bag"
+    ic " > Finding $bag_file_pattern:"
+    reply=($(find $search_directory -type f -name $bag_file_pattern -exec basename {} \;))
+    # indirect:
+    set -A "$placeholder_name" "${reply[@]}"
+}
+
+function find_all_directories() {
+    # find bag file:
+    local placeholder_name=$1 # returns
+    local search_directory=$2 #~/.ros/bag_replay_recorder_files
+    local bag_file_pattern=$3 #"session_*"
+    ic " > Finding $bag_file_pattern:"
+    reply=($(find $search_directory -type d -name $bag_file_pattern -exec basename {} \;))
+    # indirect:
+    set -A "$placeholder_name" "${reply[@]}"
 }
