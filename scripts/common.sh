b2fefb21c686ab57e01e980f05a76e56b95a5b1c
Tim <timvanmeij@uwaterloo.ca>
2023-09-15 13:31:09 -0400

    name change of branch

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,7 +2,7 @@
 # TIPS: to debug the script, append first line with: `#!/usr/bin/zsh -x` (in specific zsh or in common.sh)
 #################################################################
 ## USER PARAM: ##
-export UWARL_catkin_ws_branch="waterloo_steel/universal/ros1/tim/sep-2023"
+export UWARL_catkin_ws_branch="universal/ros1/tim/sep-2023"
 
 #################################################################
 #    ## SUMMIT Side:
